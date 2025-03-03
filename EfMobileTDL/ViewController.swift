aea6a8797a6d9180837054c36bd4bce710eea13d
Kirill_Sklyarov <k.sklyarov@yandex.ru>
2025-03-03 19:17:35 +0300

    Initial Commit

---
+++ EfMobileTDL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EfMobileTDL
+//
+//  Created by Kirill Sklyarov on 03.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
